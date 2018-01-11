a0b0912582c864998560e550538e69c52e3e81d8
Uthaeus <Uth@github.com>
2018-01-11 13:23:48 -0700

    Implemented labels with status values on index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,9 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
+    
     @posts = Post.posts_by current_user
+    
   end
 
   def new
