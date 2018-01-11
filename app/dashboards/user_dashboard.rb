bab40d545d93f98c0ef9058c9416d626fc0f428b
Uthaeus <Uth@github.com>
2018-01-11 10:45:44 -0700

    Refactored post_spec tests

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -16,7 +16,6 @@ class UserDashboard < Administrate::BaseDashboard
     created_at: Field::DateTime.with_options(searchable: false),
     updated_at: Field::DateTime.with_options(searchable: false),
     type: Field::String.with_options(searchable: false),
-    phone: Field::String.with_options(searchable: false),
   }.freeze
 
   COLLECTION_ATTRIBUTES = [
@@ -28,7 +27,6 @@ class UserDashboard < Administrate::BaseDashboard
   SHOW_PAGE_ATTRIBUTES = [
     :posts,
     :email,
-    :phone,
     :sign_in_count,
     :current_sign_in_at,
     :last_sign_in_at,
@@ -46,6 +44,5 @@ class UserDashboard < Administrate::BaseDashboard
     :password,
     :first_name,
     :last_name,
-    :phone,
   ].freeze
 end
