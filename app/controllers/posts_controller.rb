11b60f7a848ad24b19ab8ec7be08739326f6de73
Uthaeus <Uth@github.com>
2018-01-23 14:36:11 -0700

    Implemented approval from admin homepage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :approve]
 
   def index
     if current_user.type == 'AdminUser'
@@ -7,6 +7,12 @@ class PostsController < ApplicationController
     else
       @posts = Post.posts_by(current_user).page(params[:page]).per(10)
     end
+  end
+
+  def approve
+    authorize @post 
+    @post.approved!
+    redirect_to root_path, notice: "The post has been approved"
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,17 @@ Rails.application.routes.draw do
   resources :audit_logs, except: [:new, :edit, :destroy]
   namespace :admin do
     resources :users
-    resources :posts
+    resources :posts 
     resources :admin_users
 
     root to: "users#index"
   end
 
-  resources :posts
+  resources :posts do 
+    member do 
+      get :approve
+    end
+  end
   devise_for :users, skip: [:registration]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static#homepage'
