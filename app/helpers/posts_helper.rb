9de3d70ac577f8dc8f0174cdea2f76e7a73ee970
Uthaeus <Uth@github.com>
2018-01-24 13:57:01 -0700

    Setup skeleton employee homepage

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,21 +1,21 @@
 module PostsHelper
-  def status_label status 
-    status_span_generator status
-  end
+  # def status_label status 
+  #   status_span_generator status
+  # end
 
 
 
-  private
+  # private
 
-  def status_span_generator status 
-    case status 
+  # def status_span_generator status 
+  #   case status 
 
-    when 'submitted'
-      content_tag(:span, status.titleize, class: 'label label-primary')
-    when 'approved'
-      content_tag(:span, status.titleize, class: 'label label-success')
-    when 'rejected'
-      content_tag(:span, status.titleize, class: 'label label-danger')
-    end
-  end
+  #   when 'submitted'
+  #     content_tag(:span, status.titleize, class: 'label label-primary')
+  #   when 'approved'
+  #     content_tag(:span, status.titleize, class: 'label label-success')
+  #   when 'rejected'
+  #     content_tag(:span, status.titleize, class: 'label label-danger')
+  #   end
+  # end
 end
