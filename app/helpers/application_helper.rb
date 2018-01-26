5bcd306b6e9349ffe80587f027b822a03f35a9e5
Uthaeus <Uth@github.com>
2018-01-26 11:24:42 -0700

    fixed bug in admin dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
-  def active?(path)
-    "active" if current_page?(path)
-  end
 
   def admin_types
     ['AdminUser']
+  end
+
+  def active?(path)
+    "active" if current_page?(path)
   end
 
   def employee?
